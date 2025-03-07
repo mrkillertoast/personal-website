22773e758a3e51312e770af9b1c61bc0d7d88e78
mrkillertoast <lueku.rauscher@gmail.com>
2025-03-07 07:49:49 +0100

    moved to tailwindcss 3 & shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
-	css: ['~/assets/css/tailwind.css'],
-	vite: {
-		plugins: [
-			tailwindcss(),
-		],
-	},
+	css: [ '~/assets/css/tailwind.css' ],
+	modules: [ '@nuxtjs/tailwindcss', 'shadcn-nuxt' ],
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: '',
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: './components/ui'
+	}
 });
