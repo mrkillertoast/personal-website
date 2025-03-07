ec95542baefd6b5af19728b1aecb4f0a0cd1895b
mrkillertoast <lueku.rauscher@gmail.com>
2025-03-07 13:06:26 +0100

    Finish porting AI Template - added new styles - added components to App.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <Header />
+   <!-- <Header />
+    <Hero />
+    <Experience />
+    <Projects />
+    <Contact />
+    <Footer />-->
+    <div class="flex flex-col content-center min-h-screen">
+      <Header />
+      <main class="flex-1">
+        <Hero />
+        <Experience />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
