735c62f27ea06397e1e07e08690525fbaaceb966
mrkillertoast <lueku.rauscher@gmail.com>
2025-03-07 07:50:53 +0100

    added Header component to app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Header />
   </div>
 </template>
+<script setup lang="ts">
+import Header from "~/components/Header.vue";
+</script>
