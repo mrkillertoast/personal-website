5d372aa8d5285ed8d949a10589c836b531ca94ad
mrkillertoast <lueku.rauscher@gmail.com>
2025-03-07 13:13:14 +0100

    fixed content centering - removed commented components on app.vue

--- app.vue
+++ app.vue
@@ -1,21 +1,13 @@
 <template>
-  <div>
-   <!-- <Header />
-    <Hero />
-    <Experience />
-    <Projects />
-    <Contact />
-    <Footer />-->
-    <div class="flex flex-col content-center min-h-screen">
-      <Header />
-      <main class="flex-1">
-        <Hero />
-        <Experience />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+  <div class="flex flex-col content-center min-h-screen">
+    <Header/>
+    <main class="flex-1">
+      <Hero/>
+      <Experience/>
+      <Projects/>
+      <Contact/>
+    </main>
+    <Footer/>
   </div>
 </template>
 <script setup lang="ts">
