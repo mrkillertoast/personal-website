b0627ce8fe6b7b0b336ae87cbe8ca24a47056d8f
mrkillertoast <lueku.rauscher@gmail.com>
2025-03-08 11:41:11 +0100

    added layouts - moved from app to index page - added default layout

--- app.vue
+++ app.vue
@@ -1,17 +1,7 @@
 <template>
-  <div class="flex flex-col content-center min-h-screen">
-    <Header/>
-    <main class="flex-1">
-      <Hero/>
-      <Experience/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
-    </main>
-    <Footer/>
-  </div>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
 <script setup lang="ts">
-import Header from "~/components/Header.vue";
-import Skills from "~/components/Skills.vue";
 </script>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col content-center min-h-screen">
+    <Header/>
+    <main class="flex-1">
+      <slot/>
+    </main>
+    <Footer/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
