91f6b4486c25167443c756e844ecdc6f282aca54
mrkillertoast <lueku.rauscher@gmail.com>
2025-03-09 12:51:12 +0100

    add @nuxt/icon - @iconify-json/cib dependencies; - update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-	compatibilityDate: '2024-11-01',
-	devtools: { enabled: true },
-	css: [ '~/assets/css/tailwind.css', '~/assets/css/main.css' ],
-	modules: [ '@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/content' ],
-	shadcn: {
-		/**
-		 * Prefix for all the imported component
-		 */
-		prefix: '',
-		/**
-		 * Directory that the component lives in.
-		 * @default "./components/ui"
-		 */
-		componentDir: './components/ui'
-	}
+				compatibilityDate: '2024-11-01',
+				devtools: { enabled: true },
+				css: [ '~/assets/css/tailwind.css', '~/assets/css/main.css' ],
+				modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/content', '@nuxt/icon'],
+				shadcn: {
+								/**
+									* Prefix for all the imported component
+									*/
+								prefix: '',
+								/**
+									* Directory that the component lives in.
+									* @default "./components/ui"
+									*/
+								componentDir: './components/ui'
+				}
 });
